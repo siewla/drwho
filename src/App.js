7bdc63fbad53ec089863ab392612e13beef91390
siewla <siewlapang@gmail.com>
2020-07-15 21:03:20 +0800

    toggle changeIt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+export class App extends Component {
+  constructor (){
+    super()
+    this.state= {
+      tardis: {
+        name: 'Time and Relative Dimension in Space',
+        caps: false,
+      }
+    }
+  }
+
+  changeIt (text) {
+    if (this.state.tardis.caps) {
+      this.setState({
+        tardis: {
+          name: text.toLowerCase(),
+          caps: false
+        }
+      })
+    } else {
+      this.setState({
+        tardis: {
+          name: text.toUpperCase(),
+          caps: true
+        }
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3 onClick={()=>this.changeIt(this.state.tardis.name)}>{this.state.tardis.name}</h3>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
